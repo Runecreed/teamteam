b57206458a91d9f610836f6c3d0fb8625f22744c
Runecreed <kevin.cleijne@live.nl>
2018-01-09 16:50:03 +0100

    Trips model

--- railmate/models.py
+++ railmate/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+import datetime
+
 
 # Create your models here.
+
+class Trips(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)  # Reference user table
+    source = models.CharField()
+    destination = models.CharField()
+    date = models.DateField()
+    time = models.CharField(max_length='4')  # store hh:mm timestamp, parse later into DateTime object if need be
+    deviation = models.IntegerField(blank=True,
+                                    null=True)  # possible deviation, not required and stored as NULL if not given
+    subscription = models.TextField()
+    compensation = models.TextField()
+    companions = models.IntegerField(verbose_name="Amount of Passengers coming with", default=0)
+    max_companions = models.IntegerField(verbose_name="Total amount of extra passengers allowed",
+                                         default=4)  # amount of people allowed with
+
+    def spaceLeft(self):
+        if (self.companions >= self.max_companions):
+            return False
+        else:
+            return True
+
+    def __str__(self):      #verbose reporting of this entry
+        return self.user + ' travels from ' + self.source + ' to ' + self.destination + ' on ' + \
+               self.date.strftime('%d/%m/%y') + ' at ' + self.time + ' with ' + str(self.companions) + ' passengers'
+
