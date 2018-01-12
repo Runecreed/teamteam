b51823e31a4500d4503937e81c7a597f2d653d75
Julia <j.a.claassen@student.tue.nl>
2018-01-12 12:24:16 +0100

    poging tot uploaden naar de database

--- railmate/forms.py
+++ railmate/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django import forms
-from railmate.models import Profile
+from railmate.models import Profile, Trip
 
 
 class UserForm(forms.ModelForm):
@@ -8,7 +8,14 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ('first_name', 'last_name', 'email')
 
+
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('avatar', 'birth_date')
+
+
+class TripForm(forms.ModelForm):
+    class Meta:
+        model = Trip
+        fields = ('source', 'destination', 'date', 'time', 'deviation', 'subscription', 'compensation')
